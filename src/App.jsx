47216d99cc106ea3f6796d3d53f32da0bfcedebc
Uvaishbelim <162904842+Uvaishbelim@users.noreply.github.com>
2026-05-06 16:42:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Navbar from './Navbar'
+import HeroSection from './HeroSection'
+import FeatursSection from './FeatursSection'
+import FruitsShop from './FruitsShop'
+import Featurs from './Featurs'
+import VesitableShop from './VesitableShop'
+import BannerSection from './BannerSection'
+import BestsalerProduct from './BestsalerProduct'
+import Fact from './Fact'
+import Tastimonial from './Tastimonial'
+import Footer from './Footer'
+import Copyright from './Copyright'
+import Spinner from './Spinner'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <Navbar/>
+      <HeroSection/>
+      <FeatursSection/>
+      <FruitsShop/>
+      <Featurs/>
+      <VesitableShop/>
+      <BannerSection/>
+      <BestsalerProduct/>
+      <Fact/>
+      <Tastimonial/>
+      <Footer/>
+      <Copyright/>
+      
+    </>
+  )
+}
+
+export default App
